f3212d76e600e664801670fc6e72ca094ab03f63
taIshikawa <yatono0314@gmail.com>
2023-04-03 15:29:57 +0900

    fix

--- docs/server/api/api.ts
+++
@@ -1,7 +0,0 @@
-export * from './addresses.service';
-import { AddressesService } from './addresses.service';
-export * from './auth.service';
-import { AuthService } from './auth.service';
-export * from './users.service';
-import { UsersService } from './users.service';
-export const APIS = [AddressesService, AuthService, UsersService];
